e357293f4b5ca90c0fb4e35f59fa49fdb9bd10fa
Gaji Yaqub <gajiyakub6@gmail.com>
2025-10-02 17:55:03 -0700

    Completed Working on the Buttons, Begin, Pause, Stop

--- productivity.js
+++ productivity.js
@@ -7,39 +7,111 @@ let timer = 0;
 hello.innerHTML += `Hello ${savedName}`
 console.log(hello)
 
-let beginButton = document.querySelector('.js-begin-button')
+const beginButton = document.querySelector('.js-begin-button')
+const pauseButton = document.querySelector('.js-pause-button')
+const stopButton = document.querySelector('.js-stop-button')
+
+
+
 let shownTime = document.querySelector('.js-shown-time')
 let timeSelected = document.querySelector('.js-time-selected')// We are assigning this in here so we won't be getting the value of this immediately the website opens.
 
-beginButton.addEventListener('click', )
+document.body.addEventListener('keydown', (event)=> {
+  if(event.key === 'p'){
+    pause();
+  }
+  else if(event.key === 'b'){
+    begin();
+  }
+  else if(event.key === 'Enter'){
+    event.preventDefault();
+    begin();
+  }
+  console.log(event.key)
+})
+
+
+
+beginButton.addEventListener('click', begin)
+pauseButton.addEventListener('click', ()=>{
+  if(pauseButton.classList.contains('js-continue-clicked')){
+    continueNow();
+  }
+  else {
+    pause(); // If not, leave at pause.
+  }
+})
+stopButton.addEventListener('click', ()=> {
+  stop();
+})
 
 
 
 function begin(){
-  console.log(timeSelected.value)
-  timer = Number(timeSelected.value) * 60 * 60 // This is to make it in form of hours.
+  beginButton.classList.add('js-begin-clicked')
+  setTimeout(() => {
+    beginButton.classList.remove('js-begin-clicked')
+  }, 200);
+  timer = Number(timeSelected.value) * 60 * 60// This is to make it in form of hours.
+  console.log(timer)
+  setTimeout(()=>{
+    timeSelected.value = ''
+  }, 300)
   shownTime.innerHTML = `Productivity Started: ${formatTime(timer)}`
-
   clearInterval(intervalId)
   intervalId = setInterval(countdown, 1000)
 }
 
 
 
+function pause(){
+  pauseButton.classList.add('js-pause-clicked')
+  setTimeout(() => {
+      pauseButton.classList.remove('js-pause-clicked')
+      pauseButton.classList.add('js-continue-clicked')
+    }, 200);
+  shownTime.innerHTML = `Time Paused: ${formatTime(timer)} seconds`
+  clearInterval(intervalId)
+  pauseButton.innerHTML = 'Continue'
+  // pauseButton.classList.add('js-continue-button')
+}
 
-timeSelected.addEventListener('keydown', (event)=> {
-  if (event.key === 'Enter'){
-    event.preventDefault() // This is used to stop the site from reloading which actually happens when the "Enter" key is being pressed.
-    begin();
-  }
-})
+function continueNow(){
+  pauseButton.classList.remove('js-continue-clicked')
+  pauseButton.innerHTML = 'Pause'
+  intervalId = setInterval(countdown, 1000)
+  console.log(intervalId)
+  shownTime.innerHTML = `Timer Continued ${formatTime(timer)}`
+}
 
 
 
+function stop(){
+  stopButton.classList.add('js-stop-clicked')
+  setTimeout(()=>{
+    shownTime.innerHTML = ''
+  }, 5000)
+  setTimeout(()=>{
+    stopButton.classList.remove('js-stop-clicked')
+  }, 200)
+  clearInterval(intervalId)
+  shownTime.innerHTML = `Wow, I can't believe you would quit early at ${formatTime(timer)}`
+}
+
+
+let timerchecker = 0;
 function countdown (){
   if(timer >= 0){
     timer --;
-    shownTime.innerHTML = `Time left: ${formatTime(timer)} seconds`
+    timerchecker ++;
+    if (timerchecker === 3600/*This represents an hour*/){
+      pause();
+      shownTime.innerHTML = `You have used ${formatTime(timer)}hrs. Take a rest`
+    }
+
+    else {
+      shownTime.innerHTML = `Time left: ${formatTime(timer)} seconds`
+    }
   }
 
   else{
